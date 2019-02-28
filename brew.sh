eaa87576c618f4441b9d0746ce67f5e517c4b1be
Tasdik Rahman <tasdik.rahman@go-jek.com>
2019-02-28 19:48:14 +0530

    add brew.sh

---
+++ brew.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+# Install Bash 4.
+brew install bash
+brew install bash-completion2
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/bash";
+fi;
+
+# Install `wget` with IRI support.
+brew install wget --with-iri
+
+# Install GnuPG to enable PGP-signing commits.
+brew install gnupg
+
+# Install more recent versions of some macOS tools.
+brew install vim --with-override-system-vi
+brew install grep
+brew install openssh
+brew install screen
+brew install tmux
+
+# Install some CTF tools; see https://github.com/ctfs/write-ups.
+brew install binutils
+brew install nmap
+brew install tcpflow
+brew install tcpreplay
+brew install telnet
+brew install tcptrace
+brew install ucspi-tcp # `tcpserver` etc.
+
+# Install other useful binaries.
+brew install ack
+brew install git
+brew install ssh-copy-id
+brew install tree
+
+# Install Ruby related tools
+brew install rbenv
+brew install gem-rehash
+brew install default-gems
+brew install gemset
+
+# Install terraform related tools
+brew install tfenv
+
+# Remove outdated versions from the cellar.
+brew cleanup
